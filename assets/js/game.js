3b482cab8ba9013f372e42cc297978bf729b1996
analizajb <96861913+analizajb@users.noreply.github.com>
2022-03-31 15:56:50 -0700

    interaction w player thru browser alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,44 @@
 var playerName = window.prompt("What is your robot's name?");
-// What is this?
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
+// You can also log multipe values at once lie this
+console.log(playerName, playerAttack, playerHealth);
 
-//this will do math and log 20
-console.log(10 + 10);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// what is this?
-console.log("our robot's name is " + playerName);
+var fight = function() {
+    // This alerts players the round is starting.
+    window.alert("Welcome to Roobt Gladiators!");
 
-function fight () {
-    window.alert("The fight has begun!")
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining." 
+    );
+
+    // this is to check enemy health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died! ");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health remaining.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    //this is to check player health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died! ");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health");
+    }
 }
 
-// fight ();
+fight ();
 
